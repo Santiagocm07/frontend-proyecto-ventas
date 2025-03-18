df8575fde9cf36d102cb52109bff90aca4e31c87
Santiagocm07 <santicm50@gmail.com>
2025-03-18 10:24:02 -0500

    Se contruye la interfaz gráfica en el componente de identificación de usuarios, y tambien se agregan validaciones al formulario con mensajes personalizados

--- src/app/modulos/seguridad/identificacion-usuario/identificacion-usuario.component.ts
+++ src/app/modulos/seguridad/identificacion-usuario/identificacion-usuario.component.ts
@@ -1,11 +1,42 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-identificacion-usuario',
-  imports: [],
+  standalone: true, // Indica que este es un componente standalone
+  imports: [CommonModule, ReactiveFormsModule], // Importar ReactiveFormsModule
   templateUrl: './identificacion-usuario.component.html',
   styleUrl: './identificacion-usuario.component.css'
 })
 export class IdentificacionUsuarioComponent {
+  fGroup: FormGroup = new FormGroup({});
 
+  constructor(
+    private fb: FormBuilder,
+  ) {
+  }
+
+  ngOnInit(){
+    this.construirFormulario();
+  }
+
+  construirFormulario() {
+    this.fGroup = this.fb.group({
+      usuario: ['', [Validators.required, Validators.email]],
+      clave: ['', [Validators.required]],
+    });
+  }
+
+  identificarUsuario() {
+    if (this.fGroup.invalid) {
+      alert("Datos incompletos");
+    } else {
+      alert("Identificando...");
+    }
+  }
+
+  get obtenerFormGroup() {
+    return this.fGroup.controls;
+  }
 }
--- src/app/modulos/seguridad/seguridad.module.ts
+++ src/app/modulos/seguridad/seguridad.module.ts
@@ -6,6 +6,12 @@ import { IdentificacionUsuarioComponent } from './identificacion-usuario/identif
 import { CambioClaveComponent } from './cambio-clave/cambio-clave.component';
 import { RecuperarClaveComponent } from './recuperar-clave/recuperar-clave.component';
 import { CerrarSesionComponent } from './cerrar-sesion/cerrar-sesion.component';
+import { IdentificacionSegundofaComponent } from './identificacion-segundofa/identificacion-segundofa.component';
+import { CrearUsuarioComponent } from './usuario/crear-usuario/crear-usuario.component';
+import { EditarUsuarioComponent } from './usuario/editar-usuario/editar-usuario.component';
+import { ListarUsuarioComponent } from './usuario/listar-usuario/listar-usuario.component';
+import { EliminarUsuarioComponent } from './usuario/eliminar-usuario/eliminar-usuario.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 
 @NgModule({
@@ -13,11 +19,18 @@ import { CerrarSesionComponent } from './cerrar-sesion/cerrar-sesion.component';
   imports: [
     CommonModule,
     SeguridadRoutingModule,
+    ReactiveFormsModule,
+    FormsModule,
     
     // No es necesario declarar los componentes, solo importarlos
     IdentificacionUsuarioComponent,
+    IdentificacionSegundofaComponent,
     CambioClaveComponent,
     RecuperarClaveComponent,
+    CrearUsuarioComponent,
+    EditarUsuarioComponent,
+    ListarUsuarioComponent,
+    EliminarUsuarioComponent,
     CerrarSesionComponent
   ]
 })
