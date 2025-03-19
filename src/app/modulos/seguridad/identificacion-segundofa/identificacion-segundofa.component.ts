118784aa0f55daee00a04af931dc4f8f2b05a516
Santiagocm07 <santicm50@gmail.com>
2025-03-19 08:05:25 -0500

    Se hace la validación del 2FA, en donde se envian los datos de autenticación al backend y se obtiene una respuesta. Todo a partir del almacenamiento en local storage de la información del usuario

--- src/app/modulos/seguridad/identificacion-segundofa/identificacion-segundofa.component.ts
+++ src/app/modulos/seguridad/identificacion-segundofa/identificacion-segundofa.component.ts
@@ -1,11 +1,57 @@
 import { Component } from '@angular/core';
+import { SeguridadService } from '../../../servicios/seguridad.service';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-identificacion-segundofa',
-  imports: [],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './identificacion-segundofa.component.html',
   styleUrl: './identificacion-segundofa.component.css'
 })
 export class IdentificacionSegundofaComponent {
 
+  usuarioId: string = "";
+  fGroup: FormGroup = new FormGroup({});
+
+  constructor(
+    private servicioSeguridad: SeguridadService,
+    private fb: FormBuilder
+  ) {
+
+  }
+
+  ngOnInit(){
+    let datos = this.servicioSeguridad.obtenerDatosUsuarioLS();
+    if(datos != null) {
+      this.usuarioId = datos._id!;
+      this.construirFormulario();
+    }
+  }
+
+  construirFormulario(){
+    this.fGroup = this.fb.group({
+      codigo: ['', [Validators.required]]
+    });
+  }
+
+  validarCodigo2fa(){
+    if(this.fGroup.invalid) {
+      alert("Debe ingresar el código");
+    } else {
+      let codigo2fa = this.obtenerFormGroup["codigo"].value;
+      this.servicioSeguridad.validarCodigo2FA(this.usuarioId, codigo2fa).subscribe({
+        next: (datos:object) => {
+          console.log(datos);
+        },
+        error: (err) => {
+          console.log(err);
+        }
+      });
+    }
+  }
+
+  get obtenerFormGroup(){
+    return this.fGroup.controls;
+  }
 }
--- src/app/servicios/seguridad.service.ts
+++ src/app/servicios/seguridad.service.ts
@@ -12,10 +12,58 @@ export class SeguridadService {
 
   constructor(private http: HttpClient) { }
 
+  /**
+   * Identificar usuario
+   * @param usuario 
+   * @param clave 
+   * @returns datos del usuario validado
+   */
   identificarUsuario(usuario: string, clave: string): Observable<UsuarioModel>{
     return this.http.post<UsuarioModel>(`${this.urlBase}identificar-usuario`, {
       correo: usuario,
       clave: clave
+    });
+  }
+
+  /**
+   * Almacena los datos del usuario
+   * @param datos datos del usuario
+   */
+  almacenarDatosUsuarioIdentificado(datos: UsuarioModel): boolean{
+    let cadena = JSON.stringify(datos);
+    let datosLS = localStorage.getItem("datos-usuario");
+    if(datosLS){
+      return false;
+    } else{
+      localStorage.setItem("datos-usuario", cadena);
+      return true;
+    }
+  }
+
+  /**
+   * Busca los datos en localStorage de un usuario
+   * @returns 
+   */
+  obtenerDatosUsuarioLS(): UsuarioModel | null{
+    let datosLS = localStorage.getItem("datos-usuario");
+    if(datosLS){
+      let datos = JSON.parse(datosLS);
+      return datos;
+    } else{
+      return null;
+    }
+  }
+
+  /**
+   * Validar 2FA
+   * @param idUsuario
+   * @param codigo 
+   * @returns 
+   */
+  validarCodigo2FA(idUsuario: string, codigo: string): Observable<object>{
+    return this.http.post<object>(`${this.urlBase}verificar-2fa`, {
+      usuarioId: idUsuario,
+      codigo2fa: codigo
     });
   }
 }
