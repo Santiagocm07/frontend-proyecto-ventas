1ca3a1a0ba327379eaa2c71543485397f0fca085
Santiagocm07 <santicm50@gmail.com>
2025-03-21 10:04:51 -0500

    Se hace el funcionamiento del listado de productos, para lo cual se consume el backend y se muestra la información en una tabla con los botones de agregar, editar y eliminar

--- src/app/modulos/parametros/producto/listar-producto/listar-producto.component.ts
+++ src/app/modulos/parametros/producto/listar-producto/listar-producto.component.ts
@@ -1,11 +1,30 @@
 import { Component } from '@angular/core';
+import { ProductoModel } from '../../../../modelos/producto.model';
+import { ParametrosService } from '../../../../servicios/parametros.service';
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-listar-producto',
-  imports: [],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './listar-producto.component.html',
   styleUrl: './listar-producto.component.css'
 })
 export class ListarProductoComponent {
+  listaRegistros:ProductoModel [] = [];
+  constructor(
+    private servicioParametros: ParametrosService
+  ){}
+
+  ngOnInit(){
+    this.servicioParametros.listarRegistro().subscribe({
+      next: (datos) => {
+        this.listaRegistros = datos;
+      },
+      error: (err) => {
+        alert("Error leyendo los productos")
+      }
+    });
+  }
 
 }
