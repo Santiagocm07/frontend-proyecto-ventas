b3b34572ad1bc04979e80d2583664d5ff04c8591
Santiagocm07 <santicm50@gmail.com>
2025-03-25 14:28:12 -0500

    Se hace la paginación al listado de productos, como aun no se da con el error que se tiene en el archivo auth.interceptor.ts se hace la petición Http dentro del archivo producto.service.ts

--- src/app/modulos/parametros/producto/listar-producto/listar-producto.component.ts
+++ src/app/modulos/parametros/producto/listar-producto/listar-producto.component.ts
@@ -3,23 +3,33 @@ import { ProductoModel } from '../../../../modelos/producto.model';
 import { ParametrosService } from '../../../../servicios/parametros/producto.service';
 import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
+import { NgxPaginationModule } from 'ngx-pagination';
+import { ConfiguracionPaginacion } from '../../../../config/configuracion.paginacion';
 
 @Component({
   selector: 'app-listar-producto',
-  imports: [CommonModule, ReactiveFormsModule],
+  imports: [CommonModule, ReactiveFormsModule, NgxPaginationModule],
   templateUrl: './listar-producto.component.html',
   styleUrl: './listar-producto.component.css'
 })
 export class ListarProductoComponent {
   listaRegistros:ProductoModel [] = [];
+  pag = 1;
+  total = 0;
+  registrosPorPagina = ConfiguracionPaginacion.registroPorPagina;
   constructor(
     private servicioProductos: ParametrosService
   ){}
 
   ngOnInit(){
-    this.servicioProductos.listarRegistro().subscribe({
+    this.listarRegistros();
+  }
+
+  listarRegistros() {
+    this.servicioProductos.listarRegistrosPaginados(this.pag).subscribe({
       next: (datos) => {
-        this.listaRegistros = datos;
+        this.listaRegistros = datos.registros;
+        this.total = datos.totalRegistros;
       },
       error: (err) => {
         alert("Error leyendo los productos")
--- src/app/servicios/parametros/producto.service.ts
+++ src/app/servicios/parametros/producto.service.ts
@@ -4,13 +4,18 @@ import { ConfiguracionRutasBackend } from '../../config/configuracion.rutas.back
 import { Observable } from 'rxjs';
 import { ProductoModel } from '../../modelos/producto.model';
 import { ConfiguracionPaginacion } from '../../config/configuracion.paginacion';
+import { PaginadorProductoModel } from '../../modelos/paginador.producto.model';
+import { SeguridadService } from '../seguridad.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ParametrosService {
+  token = "";
   urlBase: string = ConfiguracionRutasBackend.urlNegocio;
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private servicioSeguridad: SeguridadService) { 
+    this.token = this.servicioSeguridad.obtenerTokenLocalStorage();
+  }
 
   /**
    * Listado de productos
@@ -18,5 +23,16 @@ export class ParametrosService {
    */
   listarRegistro(): Observable<ProductoModel[]> {
     return this.http.get<ProductoModel[]>(`${this.urlBase}producto?filter={"limit":${ConfiguracionPaginacion.registroPorPagina}}`);
+  }
+
+  listarRegistrosPaginados(pag: number): Observable<PaginadorProductoModel> {
+    let limit = ConfiguracionPaginacion.registroPorPagina;
+    let skip = (pag - 1) * limit;
+    let url = `${this.urlBase}producto-paginado?filter={"limit":${limit}, "skip":${skip}}`;
+    return this.http.get<PaginadorProductoModel>(url, {
+      headers: {
+        "Authorization": `Bearer ${this.token}`
+      }
+    });
   }
 }
